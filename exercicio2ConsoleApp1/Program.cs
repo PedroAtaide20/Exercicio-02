993b2792be99f128556e6405082b33bca6a3b7ac
Pedro Ataide Silva <Pedro Ataide Silva@DESKTOP-QNHGH3C>
2022-02-08 16:47:00 -0300

    Add project files.

---
+++ exercicio2ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercicio2ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numA, numB, numC;
+
+            Console.Write("Digite o número A: ");
+            numA = Convert.ToDouble(Console.ReadLine());
+            Console.Write(" Digite o número B: ");
+            numB = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o número C: ");
+            numC = Convert.ToDouble(Console.ReadLine());
+
+            if (numA + numB < numC)
+            {
+                Console.WriteLine("Sim,A + B é menor que C ! ! ! ");
+
+            }
+            else { Console.WriteLine("Não,A+B é maior que C ! ! !"); }
+            Console.ReadLine();
+        }
+    }
+}
